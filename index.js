35e833280372b2e6efb345953e54cbbf5ed36e79
Adam Bozzay <a.bozzay.ext@smart-reporting.com>
2023-08-22 19:50:45 +0200

    initial

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+function getScripts() {
+    const scriptFiles = fs.readdirSync('./scripts');
+    if (!scriptFiles.length) console.log('Cannot find any scripts in ./scripts directory.');
+    return scriptFiles;
+}
+
+function getIndexPage() {
+    const scripts = getScripts();
+    const buttons = scripts.map(s => `<button onclick="fetch('/${s}')">${path.parse(s).name}</button>`)
+    return `
+    <html>
+        <head>
+            <title>REST Remote Control</title>
+        </head>
+        <body>
+            ${buttons.join('\n')}
+        </body>
+    </html>`
+}
+
+const port = parseInt(process.argv[2]) || 22334;
+const server = http.createServer((req, res) => {
+    if (req.url.length <3) {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(getIndexPage());
+        return;
+    }
+    
+    const param = req.url.split('/').at(-1);
+    console.log(param);
+    if (!getScripts().includes(param)) {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    try {
+        exec(`./scripts/${param}`, console.log);
+    } catch (e) {
+        console.log(e);
+    }
+    res.writeHead(200);
+    res.end();
+});
+server.listen(port, 'localhost', () => { console.log(`Server is listening at http://localhost:${port}`); });
